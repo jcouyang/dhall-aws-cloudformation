9b98b5735b76cf9b7a5f3c719f8bed0c148d1d57
Jichao Ouyang <oyanglulu@gmail.com>
2021-07-10 15:18:53 +1000

    fix gen-readme

--- bin/gen-readme.sh
+++ bin/gen-readme.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 dhall text <<< './examples/readme.dhall ./version.dhall' > ./examples/readme.gen.dhall
-dhall-to-json < examples/readme.gen.dhall > examples/readme.gen.json
+dhall text <<< './examples/readme.dhall "master"' | dhall-to-json > examples/readme.gen.json
 dhall text < README.md.dhall > README.md
