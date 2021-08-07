784ad44216b66315e64a60c8572d16f2d4aca14a
Jichao Ouyang <oyanglulu@gmail.com>
2021-08-07 19:03:33 +1000

    push to ghcr

--- docker.nix
+++ docker.nix
@@ -1,7 +1,7 @@
 { pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/4181644d09b9.tar.gz") {} }:
 
 pkgs.dockerTools.buildImage {
-  name = "dhall-aws-cloudformation";
+  name = "ghcr.io/jcouyang/dhall-aws-cloudformation";
   config = {
     Cmd = [ "${import ./default.nix {}}/bin/dhall-aws-cloudformation-exe" ];
   };
