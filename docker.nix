c1b9e6ba8a3c55fe8aab406580a2ba2d35348ef5
Jichao Ouyang <oyanglulu@gmail.com>
2021-08-08 00:18:54 +1000

    stream layers

--- docker.nix
+++ docker.nix
@@ -1,11 +1,10 @@
 { pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/4181644d09b9.tar.gz") {} }:
-
-pkgs.dockerTools.buildImage {
+let p = import ./default.nix {};
+pkgs.dockerTools.streamLayeredImage {
   name = "ghcr.io/jcouyang/dhall-aws-cloudformation";
   tag = "latest";
   created = "now";
-  contents = import ./default.nix {};
   config = {
-    Cmd = [ "/bin/dhall-aws-cloudformation-exe" ];
+    Cmd = [ "${p}/bin/dhall-aws-cloudformation-exe" ];
   };
 }
