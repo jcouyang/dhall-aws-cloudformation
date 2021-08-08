15ca35857f385b6653fbcd76b01846d9e4ba87e0
Jichao Ouyang <oyanglulu@gmail.com>
2021-08-08 12:24:05 +1000

    minimal docker layers

--- docker.nix
+++ docker.nix
@@ -1,12 +1,20 @@
 { pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/4181644d09b9.tar.gz") {} }:
 
-let p = (import ./default.nix {});
+let haskellPackages = pkgs.haskellPackages.override {
+      overrides = haskellPackagesNew: haskellPackgesOld: rec {
+        dhall-aws-cloudformation =
+          pkgs.haskell.lib.overrideCabal
+            ( pkgs.haskell.lib.justStaticExecutables
+              ( haskellPackagesNew.callPackage ./package.nix {})
+            ) {};
+      };
+    };
 
 in pkgs.dockerTools.streamLayeredImage {
   name = "ghcr.io/jcouyang/dhall-aws-cloudformation";
   tag = "latest";
   created = "now";
   config = {
-    Cmd = [ "${p}/bin/dhall-aws-cloudformation-exe" ];
+    Cmd = [ "${haskellPackages.dhall-aws-cloudformation}/bin/dhall-aws-cloudformation-exe" ];
   };
 }
